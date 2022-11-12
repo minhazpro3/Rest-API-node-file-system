d555ccfb55bd50ce8adfed51a00fe0c9a60a1877
minhazpro3 <minhazpro1@gmail.com>
2022-11-12 16:20:10 +0600

    set delete and find id request

--- middleware/users.controller.js
+++ middleware/users.controller.js
@@ -1,8 +1,72 @@
 const { readFileSync } = require("fs");
 const config = require("../users.json");
+const fs = require("fs");
 
+// GET ALL USERS
 module.exports.getUsers = (req, res) => {
+  // sync data read
+  //   const data = readFileSync("users.json");
+  //   console.log(JSON.parse(data));
+  //     res.send(JSON.parse(data));
+
+  // async data read
+  fs.readFile("users.json", (err, data) => {
+    if (data) {
+      res.send(data);
+      res.end();
+    }
+  });
+};
+
+// ADD USERS
+module.exports.saveUsers = (req, res) => {
+  // const newUser = JSON.parse(req.body);
+  // fs.readFile("users.json", "utf8", function (err, data) {
+  //   data = JSON.parse(data);
+  //   //Step 3: append user variable to list
+  //   data.push(newUser);
+  //   console.log(data);
+  //   res.end(JSON.stringify(data));
+  // });
+};
+
+// get user with ID
+module.exports.getUsersWithId = (req, res) => {
+  const id = req.params.id;
+  // sync data read
   const data = readFileSync("users.json");
-  console.log(JSON.parse(data));
-  res.send(JSON.parse(data));
+  const users = JSON.parse(data);
+  var user = users["user" + req.params.id];
+  res.send(user);
+  res.end();
+
+  // async data read
+  // fs.readFile("users.json", (err, data) => {
+  //   if (data) {
+  //     var users = JSON.parse(data);
+  //     var user = users["user" + req.params.id];
+  //     res.send(user);
+  //     console.log(user);
+  //     res.end();
+  //   }
+  // });
+};
+
+module.exports.deleteUser = (req, res) => {
+  const id = req.params.id;
+  // sync data read
+  //   const data = readFileSync("users.json");
+  //   console.log(JSON.parse(data));
+  //     res.send(JSON.parse(data));
+
+  // async data read
+  fs.readFile("users.json", (err, data) => {
+    if (data) {
+      data = JSON.parse(data);
+      delete data["user" + id];
+      res.send(data);
+      console.log(data);
+      res.end();
+    }
+  });
 };
--- routes/v1/users.routes.js
+++ routes/v1/users.routes.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const { getUsers } = require("../../middleware/users.controller");
+const {
+  getUsers,
+  saveUsers,
+  getUsersWithId,
+  deleteUser,
+} = require("../../middleware/users.controller");
 const router = express.Router();
 
 router.route("/").get(getUsers);
+// router.route("/").post(saveUsers);
+router.route("/:id").get(getUsersWithId);
+router.route("/:id").delete(deleteUser);
 
 module.exports = router;
